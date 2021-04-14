3db74b9305812ceb701f9407aeab78a1096e8d56
nezam0003 <fhameedk.nezam@gmail.com>
2021-04-14 16:20:10 +0600

    Team: done

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,32 +1,92 @@
 import React from "react";
 import Title from "../components/Title";
 
-import team1 from "../images/clients-feed-back/feedback-1.jpg";
-import team2 from "../images/clients-feed-back/feedback-2.jpg";
-import team3 from "../images/clients-feed-back/feedback-3.jpg";
-import team4 from "../images/clients-feed-back/feedback-4.jpg";
+import team1 from "../images/team/team-1.jpg";
+import team2 from "../images/team/team-2.jpg";
+import team3 from "../images/team/team-3.jpg";
+import team4 from "../images/team/team-4.jpg";
+import team5 from "../images/team/team-5.jpg";
+import team6 from "../images/team/team-6.jpg";
+import team7 from "../images/team/team-7.jpg";
+import team8 from "../images/team/team-8.jpg";
+import team9 from "../images/team/team-9.jpg";
+import team10 from "../images/team/team-10.jpg";
+import team11 from "../images/team/team-11.jpg";
+import team12 from "../images/team/team-12.jpg";
 
 const members = [
   {
     id: 1,
     image: team1,
     name: "Nezam Khan",
+    position: "COO",
   },
 
   {
     id: 2,
     image: team2,
-    name: "Zara Abedin",
+    name: "tanvir ahmed",
+    position: "CEO",
   },
   {
     id: 3,
     image: team3,
-    name: "Jakia Reshme",
+    name: "aslam ahmed",
+    position: "adviser",
   },
   {
     id: 4,
     image: team4,
     name: "Tahsin Hasan",
+    position: "project manager",
+  },
+  {
+    id: 5,
+    image: team5,
+    name: "juboraz khan",
+    position: "design manager",
+  },
+  {
+    id: 6,
+    image: team6,
+    name: "fhameed khan",
+    position: "software enginner",
+  },
+  {
+    id: 7,
+    image: team7,
+    name: "syka afroja",
+    position: "software enginner",
+  },
+  {
+    id: 8,
+    image: team8,
+    name: "nafiz iqbal",
+    position: "software enginner",
+  },
+  {
+    id: 9,
+    image: team9,
+    name: "shafiqul islam",
+    position: "software enginner",
+  },
+  {
+    id: 10,
+    image: team10,
+    name: "imrose rony",
+    position: "software enginner",
+  },
+  {
+    id: 11,
+    image: team11,
+    name: "tayeb uddin",
+    position: "software enginner",
+  },
+  {
+    id: 12,
+    image: team12,
+    name: "emon ahmed",
+    position: "software enginner",
   },
 ];
 
@@ -37,6 +97,17 @@ const Team = () => {
         <Title title="Meet Our" subtitle="Expart" />
         <div className="container">
           <div className="row">
+            <div className="col-9 text-center mx-auto">
+              <p>
+                Here is the team behind the success of Techno valley - with a
+                strong personality and mindset of some great skills, you will
+                have a never ending relation with us.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
             {members.map((member) => {
               return (
                 <>
@@ -45,17 +116,19 @@ const Team = () => {
                     className="col-10 mx-auto mx-md-0 g-4 col-md-6 col-lg-4"
                   >
                     <div className="card h-100">
-                      <img
-                        src={member.image}
-                        alt="team"
-                        className="img-responsive img-fluid"
-                      />
+                      <div className="team-image">
+                        <img
+                          src={member.image}
+                          alt="team"
+                          className="img-responsive img-fluid"
+                        />
+                      </div>
                       <div className="card-body text-light">
-                        <h5 className="card-title text-uppercase fw-bold">
+                        <h5 className="card-title text-capitalize  fw-bold">
                           {member.name}
                         </h5>
-                        <p className="card-text text-capitalize text-muted">
-                          {member.text}
+                        <p className="card-subtitle text-uppercase text-muted">
+                          {member.position}
                         </p>
                       </div>
                     </div>
